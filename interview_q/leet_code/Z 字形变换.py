638ae33d43271eae44ea5f63d4f267cc27b6bfc6
安宁暖阳 <dongyangzhao@outlook.com>
2019-07-24 20:55:06 +0800

    力扣日常

--- interview_q/leet_code/Z 字形变换.py
+++ interview_q/leet_code/Z 字形变换.py
@@ -6,7 +6,12 @@
 def zigzag(s: str, numRows: int):
     len_s = len(s)
     if len_s == 0:
-        raise ValueError('请输入非空字符串')
+        return ''
+        # raise ValueError('请输入非空字符串')
+    if numRows==1 or len_s==1:
+        return s
+    if numRows == 2:
+        return s[::2]+s[1::2]
     tmp_li = []
     s_index = 0
     cols = get_num_col(len_s, numRows)
@@ -18,17 +23,17 @@ def zigzag(s: str, numRows: int):
                 row_tmp.append(s[s_index])
                 s_index += 1
                 if s_index == len_s:
-                    row_tmp += [' ' for _ in range(numRows-word)]
+                    row_tmp += [' ' for _ in range(numRows-word-1)]
                     break
             tmp_li.append(row_tmp)
         else:
             for _ in range(numRows):
                 row_tmp.append(' ')
-            row_tmp[space] = s[s_index]
+            row_tmp[numRows-space-1] = s[s_index]
             s_index += 1
             tmp_li.append(row_tmp)
 
-    print(tmp_li)
+    # print(tmp_li)
     # 转置后按行连接字符
     tmp_s = ''
     for row in range(numRows):
@@ -36,7 +41,6 @@ def zigzag(s: str, numRows: int):
             now_s = tmp_li[col][row]
             if now_s != ' ':
                 tmp_s += now_s
-    # print(tmp_s)
     return tmp_s
 
 
@@ -61,8 +65,8 @@ def get_num_col(len_s: int, num_row: int):
 
 
 def main():
-    # print(get_num_col(12, 3))
-    zigzag('ab', 1)
+    s = 'PAYPALISHIRING'
+    print(zigzag(s, 4))
 
 
 if __name__ == '__main__':
