f99ae80934886ad685a1485ab3a7e1d7b1ec33f2
安宁暖阳 <dongyangzhao@outlook.com>
2021-03-10 20:42:36 +0800

    小功能更新

--- interview_q/algorithm/sort.py
+++ interview_q/algorithm/sort.py
@@ -1,55 +1,121 @@
-# python 3
+#!python 3
 # utf-8
 
 
 def bubble_sort(li):
     """冒泡排序。遍历n次列表，每一次确定第i（1<i<=n）大的数。每一次遍历过程中，最大的数总是会冒泡到最后一个。"""
     n = len(li)
-    for i in range(n-1):
-        for j in range(n-i-1):
-            if li[j] >= li[j+1]:
+    for i in range(n - 1):
+        for j in range(n - i - 1):
+            if li[j] >= li[j + 1]:
                 tmp = li[j]
-                li[j] = li[j+1]
-                li[j+1] = tmp
+                li[j] = li[j + 1]
+                li[j + 1] = tmp
     return li
 
 
 def quick_sort(li, p, r):
     """算法导论 快速排序 递归部分"""
     if p < r:
-        q = partition(li, p, r)
+        q = partition_by_start(li, p, r)
         quick_sort(li, p, q - 1)
         quick_sort(li, q + 1, r)
 
 
 def partition(li, start, end):
     """算法导论 快速排序 划分部分"""
-    i = start-1
+    i = start - 1
     for j in range(start, end):
         if li[j] <= li[end]:
             i += 1
             li[i], li[j] = li[j], li[i]
-    li[end], li[i+1] = li[i+1], li[end]
-    return i+1
+    li[end], li[i + 1] = li[i + 1], li[end]
+    return i + 1
+
+
+def partition_up(li, start, end):
+    """算法导论 快速排序 划分部分 改版"""
+    i = start
+    for j in range(start, end):
+        if li[j] <= li[end]:
+            li[i], li[j] = li[j], li[i]
+            i += 1
+    li[end], li[i] = li[i], li[end]
+    return i
 
 
 def partition_by_start(li, start, end):
-    """把开始元素作为对比元素的划分方法，根据partition改编"""
-    i = start
-    for j in range(start+1, end):
-        if li[j] <= li[start]:
-            i += 1
+    """把开始元素作为对比元素的划分方法，根据partition改编。得到从大到小的有序结果。
+    一开始我想把开始元素做对比元素，然后还是得到从小到大的结果，发现这样会增加操作次数。"""
+    i = end+1
+    j = end
+    while j > start:
+        if li[j] < li[start]:
+            i -= 1
             li[i], li[j] = li[j], li[i]
-    li[start], li[i] = li[i], li[start]
-    return i
+        j -= 1
+    li[i-1], li[start] = li[start], li[i-1]
+    return i-1
+
+
+def quick_sort_non_recursion(li, start, end):
+    stack = [start, end]
+    while len(stack) != 0:
+        end = stack.pop()
+        start = stack.pop()
+        p = partition_up(li, start, end)
+        if start < p-1:
+            stack.append(start)
+            stack.append(p-1)
+        if p+1 < end:
+            stack.append(p+1)
+            stack.append(end)
+
+
+def is_ascend(li):
+    for i in range(len(li)-1):
+        if li[i] > li[i+1]:
+            return False
+    return True
+
+
+def heap_sort(li):
+    """堆排序。
+    思想特别简单，以大根堆为例。
+    每次把二叉树里最大的元素移动到根节点，交换根节点与最后一位。
+    再从除了最后一位的元素里，找到最大的元素，移动到根节点，交换根节点与倒数第二位。
+    ······
+    当二叉树只有一个根节点时，排序完成。
+    """
+    end = len(li)-1
+    while end > 0:
+        build_heap(li, end)
+        li[0], li[end] = li[end], li[0]
+        end -= 1
+
+
+def build_heap(li, end):
+    """建立大根堆"""
+    parent_tmp = -1
+    for i in range(end):
+        parent = int((end-i-1)/2)
+        if parent != parent_tmp:
+            if parent*2+1 < end+1:
+                if li[parent*2+1] > li[parent]:
+                    li[parent*2+1], li[parent] = li[parent], li[parent*2+1]
+            if parent*2+2 < end+1:
+                if li[parent*2+2] > li[parent]:
+                    li[parent*2+2], li[parent] = li[parent], li[parent*2+2]
+            parent_tmp = parent
 
 
 def main():
     c = [-10, -5, 0, 5, 3, 10, 15, -20, 25]
     # c = [2, 8, 7, 1, 3, 5, 6, 4]
+    # c = [-5, -20, 0, 6, 1]
     quick_sort(c, 0, len(c) - 1)
     print(c)
 
-    
+
 if __name__ == "__main__":
     main()
---
+++ interview_q/plot_try/plt_draw.py
@@ -0,0 +1,131 @@
+# coding: utf-8
+# python 3
+# 画图
+
+import sys
+import os
+import matplotlib.pyplot as plt
+
+
+def draw_scatter(data: list, title_name: str= ""):
+    """散点图"""
+    plt.figure(dpi=200)
+    plt.title(title_name)
+    num_dots = len(data)
+
+    plt.scatter(range(num_dots), data)
+    # plt.xticks(num_dots/100)
+    plt.grid(axis="y")  # 显示网格线，且只显示y轴的线
+    plt.show()
+
+
+def draw_plot(data: list, title_name: str=""):
+    """折线图"""
+    plt.figure(dpi=150)
+    plt.title(title_name)   # title() 要写在figure()后面
+    num_dots = len(data)
+    plt.plot(range(num_dots), data)
+    plt.xticks(range(num_dots))
+    # plt.grid()      # 同时显示x与y轴的网格线
+    plt.grid(axis="y", linestyle="--")
+    plt.show()
+
+
+def draw_bar(data: dict, title_name=""):
+    """柱状图"""
+    font = {"family": "FangSong"}
+    # 设置字体，虽然感觉只改sans-serif，但应该够了
+    plt.rcParams['font.sans-serif'] = ['SimHei']
+    plt.title(title_name)
+    plt.bar(data.keys(), data.values())
+    # plt.xlabel("分段", fontdict=font)  # xlabel 指对x轴的介绍
+    for key in data.keys():
+        plt.text(key, data[key], data[key], ha='center', fontdict=font)
+    plt.show()
+
+
+def get_percent(numerator, denominator):
+    answer = round(numerator*1.0/denominator, 10)
+    return str(answer*100)+"%"
+
+
+def get_percent_num(numerator, denominator):
+    return round(numerator*1.0/denominator, 5)
+
+
+def draw_plot_diy_xy(data: list, x_tick, y_tick, title_name: str=""):
+    """折线图，定制x轴与y轴上的刻度数字"""
+    plt.figure(dpi=150)
+    plt.title(title_name)   # title() 要写在figure()后面
+    num_dots = len(data)
+    plt.plot(range(num_dots), data)
+    plt.xticks(x_tick)
+    plt.yticks(y_tick)
+    plt.grid(linestyle="--")
+    plt.show()
+
+
+def draw_parallel_bar(data: list, x_tick, label, scale, title_name=""):
+    """画并列柱状图
+    :param data         必须是[[],[]] 这种元素为“子列表[]” 的list 的形式，“子列表[]” 里的元素就是并列在一起的柱子
+    :param title_name   图片标题
+    :param x_tick       x轴显示的刻度文字
+    :param label        与“子列表[]” 长度必须一样，表示并列在一起的柱子中各个柱子的名字
+    :param scale        对某个柱子的缩放比例。与“子列表[]” 长度必须一样
+    """
+    import numpy
+    plt.figure(dpi=200)
+    plt.title(title_name)
+
+    n = len(label)
+    if n != len(data[0]):
+        print("函数输入参数label 长度与数据不一致!")
+        return
+    if len(scale) != len(data[0]):
+        print("函数输入参数scale 长度与数据不一致!")
+        return
+
+    color_li = ["yellow", "red", "blue", "green", "purple", "brown", "white", "cyan"]
+    total_width = 0.8  # 在x轴上总共占用的宽度
+    lc = len(color_li)
+
+    np_data = numpy.array(data)
+    cor = range(len(np_data))  # x坐标
+    width = total_width / n
+    font = {"family": "Fira Code", "size": "6"}
+    for i in range(n):
+        x_cor = [j+width*i for j in cor]
+        y_cor = np_data[:, i]
+        if i == 0:
+            plt.bar(x_cor, y_cor/scale[i], width=width, label=label[i], fc=color_li[i % lc], tick_label=x_tick)
+        else:
+            plt.bar(x_cor, y_cor/scale[i], width=width, label=label[i], fc=color_li[i % lc])
+        for k in range(len(x_cor)):
+            plt.text(x_cor[k], y_cor[k]/scale[i], y_cor[k], ha='center', fontdict=font)
+    plt.yticks([])  # 因为使用了尺度缩放，所以不显示y轴坐标
+    plt.legend()  # 显示对每个柱子的说明，就是把label中的名字与图里的柱子对应起来的小说明
+    plt.show()
+
+
+def main():
+    # draw_bar({})
+    # li = [x+1 for x in range(10)]
+    # draw_plot(li)
+
+    num_list = [[1.5, 10], [0.6,20], [7.8,30], [6,10]]
+    # print(num_list)
+    # import numpy
+    # nl = numpy.array(num_list)
+    # print(nl[:,1])
+    x_tick = ['Monday', 'Tuesday', 'Friday', 'Sunday']
+    label = ["boy", "girl"]
+    scale = [1, 5]
+    draw_parallel_bar(num_list, x_tick, label, scale)
+
+
+if __name__ == "__main__":
+    import time
+    print('-' * 15, 'Start', time.ctime(), '-' * 15, '\n')
+    now_path = os.getcwd()
+    # print(now_path)
+    sys.path.append(now_path)
